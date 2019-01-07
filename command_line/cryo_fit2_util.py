46d7e1a472e4cec0235cbdbf7222fb007722bca6
doonam at his newest personal laptop <doonam@lanl.gov>
2019-01-07 12:23:20 -0600

    refactored with util

---
+++ command_line/cryo_fit2_util.py
@@ -0,0 +1,100 @@
+from __future__ import division, print_function
+import iotbx.phil, libtbx
+from libtbx import group_args
+from libtbx.utils import Sorry
+from iotbx import map_and_model
+import mmtbx.utils, os
+from mmtbx.dynamics import simulated_annealing as sa
+import shutil
+
+def calculate_cc(map_data, model, resolution):
+    xrs = model.get_xray_structure()
+    fc = xrs.structure_factors(d_min = resolution).f_calc()
+    f_map = fc.structure_factors_from_map(
+      map            = map_data,
+      use_scale      = True,
+      anomalous_flag = False,
+      use_sg         = False)
+    return fc.map_correlation(other = f_map)
+####################### end of calculate_cc function
+
+
+def know_how_much_map_origin_moved(map_file_name):
+    print ("Know how much map origin moved")
+    
+    # Compute a target map
+    from iotbx import ccp4_map
+    ccp4_map = ccp4_map.map_reader(map_file_name)
+    print ("\tMap read from", str(map_file_name))
+    target_map_data = ccp4_map.map_data()
+    
+    #print "\tdir(): ", dir(ccp4_map)
+    # acc = target_map_data.accessor() # not used, but keep for now
+    print ("\ttarget_map_data.origin():", str(target_map_data.origin()))
+
+    emmap_x0 = target_map_data.origin()[0] # tRNA: 0, nucleosome: -98    
+    emmap_y0 = target_map_data.origin()[1] # tRNA: 0, nucleosome: -98
+    emmap_z0 = target_map_data.origin()[2] # tRNA: 0, nucleosome: -98
+    
+    if (emmap_x0 == 0 and emmap_y0 == 0 and emmap_z0 == 0):
+        return "origin_is_all_zero"
+    else:
+        print ("\t\tccp4_map.unit_cell_parameters", str(ccp4_map.unit_cell_parameters))
+        a,b,c = ccp4_map.unit_cell_parameters[:3]
+        widthx = a/target_map_data.all()[0]
+        print ("\t\twidthx", str(widthx)) # with nucleosome, I confirmed that widthx doesn't change by origin shift
+        
+        shifted_in_x = target_map_data.origin()[0]
+        shifted_in_y = target_map_data.origin()[1]
+        shifted_in_z = target_map_data.origin()[2]
+        return widthx, shifted_in_x, shifted_in_y, shifted_in_z     
+############## end of know_how_much_map_origin_moved function
+
+
+def return_to_origin_of_pdb_file(input_pdb_file_name, widthx, move_x_by, move_y_by, move_z_by):
+    move_x_by = move_x_by*widthx
+    move_y_by = move_y_by*widthx
+    move_z_by = move_z_by*widthx
+    print ("move_x_by:",move_x_by)
+    print ("move_y_by:",move_y_by)
+    print ("move_z_by:",move_z_by)
+    f_in = open(input_pdb_file_name)
+    
+    output_pdb_file_name = input_pdb_file_name[:-4] + "_origin_returned" + ".pdb"
+    
+    f_out = open(output_pdb_file_name, "w")
+    for line in f_in:
+      if line[0:4] == "ATOM" or line[0:6] == "HETATM":
+        x_coor_former = line[30:38]
+        new_x_coor = str(float(x_coor_former) + float(move_x_by))
+        new_x_coor = str(round(float(new_x_coor), 3))
+        splited = new_x_coor.split(".")
+        multi_before_period = 4-len(splited[0])
+        multi_after_period = 3-len(splited[1])
+        new_line = line[:30] + multi_before_period*" "+splited[0] + "." + splited [1]+multi_after_period*" "
+        
+        y_coor_former = line[38:46]
+        new_y_coor = str(float(y_coor_former) + float(move_y_by))
+        new_y_coor = str(round(float(new_y_coor), 3))
+        splited = new_y_coor.split(".")
+        multi_before_period = 4-len(splited[0])
+        multi_after_period = 3-len(splited[1])
+        new_line = new_line + multi_before_period*" "+splited[0] + "." + splited [1]+multi_after_period*" "
+        
+        z_coor_former = line[46:54]
+        new_z_coor = str(float(z_coor_former) + float(move_z_by))      
+        new_z_coor = str(round(float(new_z_coor), 3))
+        splited = new_z_coor.split(".")
+        multi_before_period = 4-len(splited[0])
+        multi_after_period = 3-len(splited[1])
+        new_line = new_line + multi_before_period*" "+splited[0] + "." + splited [1]+multi_after_period*" " \
+              + line[54:]
+        f_out.write(new_line)
+        
+      elif line[0:3] == "TER":
+        f_out.write(line)
+    f_in.close()
+    f_out.close()
+    command = "mv " + output_pdb_file_name + " " + input_pdb_file_name
+    libtbx.easy_run.call(command)
+################################## end of return_to_origin_of_pdb_file ()
